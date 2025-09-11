e5e4ced828312683a45b7c9a418257874927b1c8
midega-g <midegageorge2@gmail.com>
2025-09-11 08:38:37 +0300

    created ecs cluster and task definition

---
+++ ecs-task-definition.tf
@@ -0,0 +1,35 @@
+resource "aws_ecs_task_definition" "client" {
+  family = "${var.default_tags.project}-client"
+  requires_compatibilities = ["FARGATE"]
+  memory = 512
+  cpu = 256
+  network_mode = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+        name = "client"
+        image = "nicholasjackson/fake-service:vm-v0.26.2"
+        cpu = 0
+        essential = true
+
+        portMappings = [
+            {
+                containerPort = 9090
+                hostPort = 9090
+                protocol = "tcp"
+            }
+        ]
+
+        environment = [
+            {
+                name = "NAME"
+                value = "client"
+            },
+            {
+                name = "MESSAGE"
+                value = "Hello from the client"
+            }
+        ]
+    }
+  ])
+}
