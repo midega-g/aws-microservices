89bd8fa3a4319a5cdf5de0a3eb1515b9bc5af54a
midega-g <midegageorge2@gmail.com>
2025-09-11 09:52:18 +0300

    configured client ecs service and related services

---
+++ ecs-services.tf
@@ -0,0 +1,20 @@
+resource "aws_ecs_service" "client" {
+  name = "${var.default_tags.project}-client"
+  cluster = aws_ecs_cluster.main.arn
+  task_definition = aws_ecs_task_definition.client.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.client_alb_targets.arn
+    container_name = "client"
+    container_port = 9090
+  }
+
+  network_configuration {
+    subnets = aws_subnet.private.*.id
+    assign_public_ip = false
+    security_groups = [aws_security_group.ecs_client_service.id]
+  }
+
+}
